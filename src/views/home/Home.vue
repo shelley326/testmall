fffd03a4af2571f0cd57ea0f0e1c5ffdcebbfa9d
shelley326 <1993682155@qq.com>
2021-08-24 22:24:37 +0800

    '更新首页'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+//未导出，用大括号导入
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: [],
+      recommend: [],
+    };
+  },
+  created() {
+    //请求多个数据
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banners.list;
+      this.recommend = res.data.recommend.list;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
